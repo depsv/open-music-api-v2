8b1ce4fc3ff469f2919509016b23e9d29c073b1d
Debby Prasetyady <47547577+depsv@users.noreply.github.com>
2023-08-29 06:06:19 +0700

    debuging

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -12,7 +12,8 @@ class AuthenticationsHandler {
 
     async postAuthenticationHandler(request, h) {
         this._validator.validatePostAuthenticationPayload(request.payload);
-        const id = await this._usersService.verifyUserCredential(request.payload);
+        const { username, password } = request.payload;
+        const id = await this._usersService.verifyUserCredential(username, password);
 
         const accessToken = this._tokenManager.generateAccessToken({ id });
         const refreshToken = this._tokenManager.generateRefreshToken({ id });
@@ -33,10 +34,11 @@ class AuthenticationsHandler {
 
     async putAuthenticationHandler(request) {
         this._validator.validatePutAuthenticationPayload(request.payload);
+        const { refreshToken } = request.payload;
 
-        await this._authenticationsService.verifyRefreshToken(request.payload);
+        await this._authenticationsService.verifyRefreshToken(refreshToken);
 
-        const { id } = this._tokenManager.verifyRefreshToken(request.payload);
+        const { id } = this._tokenManager.verifyRefreshToken(refreshToken);
         const accessToken = this._tokenManager.generateAccessToken({ id });
         
         return {
@@ -50,9 +52,10 @@ class AuthenticationsHandler {
 
     async deleteAuthenticationHandler(request) {
         this._validator.validateDeleteAuthenticationPayload(request.payload);
+        const { refreshToken } = request.payload;
             
-        await this._authenticationsService.verifyRefreshToken(request.payload);
-        await this._authenticationsService.deleteRefreshToken(request.payload);
+        await this._authenticationsService.verifyRefreshToken(refreshToken);
+        await this._authenticationsService.deleteRefreshToken(refreshToken);
 
         return {
             status: "success",
